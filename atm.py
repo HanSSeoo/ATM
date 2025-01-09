0d03a93f118b7e3e6b5325b77b57036f776fb87a
HanSSeoo <rlagkstj9708@gmail.com>
2025-01-09 17:39:23 +0900

    🐛 영수증 보기 기능 추가
    사용자가 영수증 내역을 확인할 수 있도록 기능을 추가, 영수증 내역이 없을 경우 메세지를 출력

--- atm.py
+++ atm.py
@@ -14,15 +14,25 @@ while True:
     if num == '4':
         break
     if num == "1":
-        deposit_amount = int(input("입금할 금액을 입력해주세요 : ")) 
-        balance = balance + deposit_amount
-        receipts.append(("입금", deposit_amount, balance))
-        print(f"입금하신 금액은 {deposit_amount}원이고, 현재 잔액은 {balance}원 입니다.")
+        deposit_amount = (input("입금할 금액을 입력해주세요 : ")) 
+        if deposit_amount.isdigit() and int(deposit_amount) > 0:
+            balance = balance + int(deposit_amount)
+            receipts.append(("입금", deposit_amount, balance))
+            print(f"입금하신 금액은 {deposit_amount}원이고, 현재 잔액은 {balance}원 입니다.")
+        else:
+            prin("입금한 금액을 숫자 형태와 음수가 아닌값을 입력해주세요")
     if num == "2":
         withdraw_amount = int(input("출금할 금액을 입력해주세요 : "))
         withdraw_amount = min(balance, withdraw_amount)
         balance -= withdraw_amount
         receipts.append(("출금", withdraw_amount))
         print(f"출금하신 금액은 {withdraw_amount}원이고, 현재 잔액은 {balance}입니다.")
+    if num == "3":
+        if receipts:
+            print("===영수증===")
+            for i in receipts:
+                print(f"{i[0]}: {i[1]}원 | 잔액 : {i[2]}원")
+    else: 
+        print("영수증 내역이 없습니다.")
 
 print(f"서비스를 종료합니다. 현재 잔액은 {balance}원 입니다.")
